b7a325f8903bac4477303c78e472787cf5997b30
Mason Wray <masonwray.mw@gmail.com>
2022-09-13 19:37:11 -0500

    Create firmware project

---
+++ openbake-fw/openbake-fw.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		openbake_fw.ino
+ Created:	9/13/2022 7:27:36 PM
+ Author:	mason
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}

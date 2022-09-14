13510ae1085db73ade8a03162b33bfe214ea47f4
Mason Wray <masonwray.mw@gmail.com>
2022-09-13 20:13:12 -0500

    Initialize TFT

--- openbake-fw/openbake-fw.ino
+++ openbake-fw/openbake-fw.ino
@@ -5,15 +5,44 @@
 */
 
 #include <Adafruit_DotStar.h>
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
 
+#define TFT_DC 9
+#define TFT_CS 10
+
+enum ViewState
+{
+	SPLASH,
+	HOME,
+	PROFILE,
+};
+
+ViewState view;
 Adafruit_DotStar* ds;
+Adafruit_ILI9341* tft;
 
 void setup() {
+	// Initialize view state
+	view = SPLASH;
+
+	// Initialize RGB LED
 	ds = new Adafruit_DotStar(1, 41, 40, DOTSTAR_BGR);
 	ds->begin();
 	ds->setBrightness(1);
 	ds->setPixelColor(0, ds->gamma32(ds->Color(255, 255, 255)));
 	ds->show();
+
+	// Initialize TFT display
+	tft = new Adafruit_ILI9341(TFT_CS, TFT_DC);
+	tft->begin();
+	tft->fillScreen(ILI9341_BLACK);
+
+	// Initialize touch film driver
+
+	// Initialize thermocouple driver
+
 }
 
 void loop() {

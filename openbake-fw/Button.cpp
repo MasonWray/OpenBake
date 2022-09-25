66b66c59937c0006901071d3778b8b772d77b19d
Mason Wray <masonwray.mw@gmail.com>
2022-09-25 16:03:44 -0500

    Implement button press highlighting

--- openbake-fw/Button.cpp
+++ openbake-fw/Button.cpp
@@ -41,5 +41,53 @@ void Button::initialize()
 
 bool Button::update()
 {
-	return false;
+	using namespace Theme;
+
+	bool selected = false;
+	bool rerender = false;
+
+	TSPoint* p = ViewUtils::lerp(ts->getPoint());
+
+	if (p->z > ts->pressureThreshhold)
+	{
+		if (p->x >= bound.x && p->x <= bound.x + bound.w && p->y >= bound.y && p->y <= bound.y + bound.h)
+		{
+			rerender = pressed ? rerender : true;
+			pressed = true;
+			gap = 0;
+		}
+	}
+	else
+	{
+		if (pressed)
+		{
+			gap++;
+		}
+
+		if (gap > 8 && pressed)
+		{
+			rerender = true;
+			pressed = false;
+			selected = true;
+		}
+	}
+
+	delete p;
+
+	if (rerender)
+	{
+		uint16_t t_w; uint16_t t_h; int16_t t_x; int16_t t_y;
+		uint16_t bgc = pressed ? bg_selected : background_color;
+
+		tft->getTextBounds(name, bound.x, bound.y, &t_x, &t_y, &t_w, &t_h);
+		int text_x = bound.x + ((bound.w - t_w) / 2);
+		int text_y = bound.y + (bound.h / 2) + (t_h / 2);
+
+		tft->fillRoundRect(bound.x, bound.y, bound.w, bound.h, radius, bgc);
+		tft->drawRoundRect(bound.x, bound.y, bound.w, bound.h, radius, border_color);
+		tft->setCursor(text_x, text_y);
+		tft->print(name);
+	}
+
+	return selected;
 }
--- openbake-fw/ProfileView.cpp
+++ openbake-fw/ProfileView.cpp
@@ -34,7 +34,7 @@ void ProfileView::initialize()
 
 void ProfileView::update()
 {
-
+	test.update();
 }
 
 

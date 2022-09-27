6f22db181bb18e515c9f2b730c2c6c22e253bcf2
Mason Wray <masonwray.mw@gmail.com>
2022-09-26 21:17:12 -0500

    Create temp controller

---
+++ openbake-fw/TempController.cpp
@@ -0,0 +1,14 @@
+// 
+// 
+// 
+
+#include "TempController.h"
+
+TempController::TempController()
+{
+}
+
+TempController::~TempController()
+{
+}
+
---
+++ openbake-fw/TempController.h
@@ -0,0 +1,23 @@
+// TempController.h
+
+#ifndef _TEMPCONTROLLER_h
+#define _TEMPCONTROLLER_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+class TempController
+{
+public:
+	TempController();
+	~TempController();
+
+private:
+
+};
+
+#endif
+
